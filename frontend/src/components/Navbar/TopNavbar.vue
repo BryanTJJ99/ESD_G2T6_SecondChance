e027cb6fae3a1ce0deaba5f6ce95da3836b1201a
JustinPansacola <pansacolajustin@gmail.com>
2023-03-07 13:49:30 +0800

    Navbar

---
+++ frontend/src/components/Navbar/TopNavbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+        <div>
+            <router-link to="/chat" class="nav-link text-dark p-3 mx-3" >
+                <h3>SecondChance</h3>
+            </router-link>
+        </div>
+        
+    </nav>
+</template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../components/Home.vue'
-import Chat from '../components/Chat.vue'
+import Home from '../views/Home.vue'
+import Chat from '../views/Chat.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
