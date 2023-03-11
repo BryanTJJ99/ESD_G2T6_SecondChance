4d99b9abf7b0e1efc1900cc262756774edbbdf59
Aloysius Tan <69228664+narnia1601@users.noreply.github.com>
2023-03-11 19:12:59 +0800

    added unit testing to authentication microservices

--- backend/auth/server.js
+++ backend/auth/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const errorHandling = require('./error')
 const session = require('express-session')
-require('dotenv').config({ path: '../.env'})
+require('dotenv').config()
 
 mongoose.connect(`${process.env.MONGODB}`)
 
@@ -46,7 +46,6 @@ app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({extended: false, limit: '50mb'}))
 
 app.get("/", (req,res) => {
-    console.log(process.env)
     res.status(200).send("It works")
 })
 
@@ -67,11 +66,11 @@ app.post("/register", async (req, res) => {
             departmentId: departmentId,
             officeId: officeId
         })
-        res.status(200).send("Registered")
+        res.status(200).json("Registered")
     } catch (error) {
         console.log(error)
         const errMsg = errorHandling.errorHandling(error)
-        res.status(400).send(errMsg)
+        res.status(400).json(errMsg)
     }
 })
 
@@ -82,11 +81,13 @@ app.post("/login", async (req,res) => {
         await firebase.signInWithEmailAndPassword(firebase.getAuth(), email, password)
         const userId = await firebase.getAuth().currentUser.reloadUserInfo.localId
         req.session.uid = userId
-        res.status(200).send("Login")
+        res.status(200).json("Login")
     } catch (error) {
         const errMsg = errorHandling.errorHandling(error)
-        res.status(400).send(errMsg)
+        res.status(400).json(errMsg)
     }
 })
+
+module.exports = app
 
 app.listen(8080)
