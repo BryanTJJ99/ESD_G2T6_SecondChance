a7c44dd7522a1376dc4cef5599ac4424750e39a1
sclaireanalisa25 <claireas.2021@smu.edu.sg>
2023-03-13 22:50:02 +0800

    welcome + explore (homepage)

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,62 @@
+<style>
+.shift{
+    margin-left: 5.5rem;
+    margin-top: 1rem;
+}
+
+
+</style>
 <template>
     <div>
         <TopNavbar :organization="organization" :department="department"/>
         <Sidebar/>
         <Footer/>
+
+        <v-container>
+            <!-- Welcome back -->
+            <div class="shift">
+                <h3 class="mb-2"><i>Welcome back name,</i></h3>
+                <h5>what would you look like to do today?</h5>
+            </div>
+            <hr>
+
+            <!-- Explore -->
+            <div class="shift">
+                <h3 class="mb-2"><strong><b><i>Explore</i></b></strong></h3>
+            </div>
+            
+            <div class="shift">
+                <div class="row">
+                    <div class="col-4">
+                        <button type="button" class="btn btn-outline-success" style="border-radius:0px;padding-top:30px; padding-bottom:30px; padding-left:30px; padding-right:30px;margin:0px">icon</button>
+                        <button type="button" class="btn btn-outline-success" style="border-radius:0px;padding-top:30px; padding-bottom:30px; padding-left:80px; padding-right:80px;margin:0px">
+                        <lead>Listings</lead></button>
+                    </div>
+                    <div class="col-4">
+                        <button type="button" class="btn btn-outline-success" style="border-radius:0px;padding-top:30px; padding-bottom:30px; padding-left:30px; padding-right:30px;margin:0px">icon</button>
+                        <button type="button" class="btn btn-outline-success" style="border-radius:0px;padding-top:30px; padding-bottom:30px; padding-left:80px; padding-right:80px;margin:0px">
+                        <lead>Listings</lead></button>
+                    </div>
+                    <div class="col-4">
+                        <button type="button" class="btn btn-outline-success" style="border-radius:0px;padding-top:30px; padding-bottom:30px; padding-left:30px; padding-right:30px;margin:0px">icon</button>
+                        <button type="button" class="btn btn-outline-success" style="border-radius:0px;padding-top:30px; padding-bottom:30px; padding-left:80px; padding-right:80px;margin:0px">
+                        <lead>Listings</lead></button>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            
+            <!-- Your listings -->
+            <div class="shift">
+                <h3><strong><b><i>Your Listings</i></b></strong></h3>
+                <small><i>Listed by your department</i></small>
+            </div>
+            
+            
+        </v-container>
     </div>
+
+
 </template>
 
 <script>
