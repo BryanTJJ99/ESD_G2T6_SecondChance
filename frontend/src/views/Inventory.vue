73ca8293c709260ddf0c018d8b390c1e4f7a7a9e
vickyyqu <112297073+vickyyqu@users.noreply.github.com>
2023-03-18 00:03:44 +0800

    edited padding

--- frontend/src/views/Inventory.vue
+++ frontend/src/views/Inventory.vue
@@ -1,121 +1,130 @@
 <template>
     <div>
-        <TopNavbar :organization="organization" :department="department"/>
-        <Sidebar/>
-        <div class="container-flex d-flex justify-content-center" style="margin-left: 4.5rem;">
-            
+        <TopNavbar :organization="organization" :department="department" />
+        <Sidebar />
+        <div class="container-flex d-flex justify-content-center p-3" style="margin-left: 4.5rem;">
+
             <div class="w-100 px-3 py-2">
-            
-            <div class="d-flex px-3 py-2 justify-content-between align-items-center">
-                <div class="row px-3 py-2">
-                    <h3 class="pt-3 ps-3 mt-2">Inventory Management</h3>
+
+                <div class="d-flex px-3 py-2 justify-content-between align-items-end">
+                  
+                    <h3>Inventory Management</h3>
+                    <button class="btn btn-dark" data-bs-toggle="modal"
+                        data-bs-target="#exampleModal"><span>Add Item</span></button>
+   
                 </div>
-                <div class="pt-4 ps-3 mt-2 px-3">
-                    <button class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal" ><span>Item</span></button>
+
+
+
+                <div class="input-group px-3 py-3">
+                    <span class="input-group-text" style="background-color:#c5dad2;" id="inputGroup-sizing-default">
+                        <p>Search</p>
+                    </span>
+                    <input type="text" class="form-control pt-3" aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-default">
+                </div>
+
+                <div class="list-group px-3">
+                    <button v-for="item in items" :key="item.id" type="button"
+                        class="list-group-item list-group-item-action d-flex justify-content-between p-3 pt-3 ps-3">
+                        <p>{{ item['name'] }}</p>
+                        <span class="badge" style="background-color:#c5dad2; color: black; width: 100px;">
+                            <p>Quantity : {{ item['Quantity'] }}</p>
+                        </span>
+                    </button>
+
+
                 </div>
             </div>
-            
-            <div class="input-group mb-3 mt-4 px-3 mx-3">
-                <span class="input-group-text" style="background-color:#c5dad2;" id="inputGroup-sizing-default">
-                    <p>Search</p>
-                </span>
+        </div>
 
-                <input type="text" class="form-control pt-3" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5 title" id="exampleModalLabel">Add Item</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="background-color:#c5dad2;" id="newItemForm">
+                                <p>Item Name</p>
+                            </span>
+                            <input type="text" v-model="newItem" class="form-control" placeholder="Item Name"
+                                aria-label="itemName" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="background-color:#c5dad2;" id="newItemQty">
+                                <p>Quantity</p>
+                            </span>
+                            <input type="number" v-model="newItemQty" class="form-control" placeholder="Qty. No"
+                                aria-label="newItemQty" aria-describedby="basic-addon1">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                        <button class="btn btn-dark" type="button" @click="addItem">Save changes</button>
+                    </div>
+                </div>
             </div>
-            <div class="list-group px-3 mx-3">
-                <button v-for="item in items" :key="item.id" type="button" class="list-group-item list-group-item-action d-flex justify-content-between p-3 pt-3 ps-3 mt-2">
-                    <p>{{item['name']}}</p>
-                    <span class="badge" style="background-color:#c5dad2; color: black; width: 100px;"> <p>Quantity : {{ item['Quantity'] }}</p></span>
-                </button>
-                
-
-            </div>
-        </div>
         </div>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5 title"  id="exampleModalLabel">Add Item</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div class="input-group mb-3">
-            <span class="input-group-text" style="background-color:#c5dad2;" id="newItemForm"><p>Item Name</p></span>
-            <input type="text" v-model="newItem" class="form-control" placeholder="Item Name" aria-label="itemName" aria-describedby="basic-addon1">
-        </div>
-        <div class="input-group mb-3">
-            <span class="input-group-text" style="background-color:#c5dad2;" id="newItemQty"><p>Quantity</p></span>
-            <input type="number" v-model="newItemQty" class="form-control" placeholder="Qty. No" aria-label="newItemQty" aria-describedby="basic-addon1">
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-        <button class="btn btn-dark" type="button" @click="addItem">Save changes</button>
-      </div>
     </div>
-  </div>
-</div>
-       
-</div>
-        
-
 </template>
 
 <script>
-    import TopNavbar from "@/components/Navbar/TopNavbar.vue";
-    import Sidebar from "@/components/Navbar/Sidebar.vue";
-    import Container from "@/components/Container.vue";
+import TopNavbar from "@/components/Navbar/TopNavbar.vue";
+import Sidebar from "@/components/Navbar/Sidebar.vue";
+import Container from "@/components/Container.vue";
 
-    export default {
-        data(){
-            return {
-                department : "Finance",
-                organization : "SMU",
-                newItem: "",
-                newItemQty: 0,
-                items:[
-                    {
-                        "name":"Ikea Dalgon",
-                        "Quantity": 4
-                    },
-                    {
-                        "name":"Swiss Candace",
-                        "Quantity": 10
-                    },
-                    {
-                        "name":"Woo Jablomi",
-                        "Quantity": 5
-                    },
-                    {
-                        "name":"Big ForceKin",
-                        "Quantity": 10
-                    },
-                    {
-                        "name":"Vicky",
-                        "Quantity": 1
-                    },
-                ]
-            }
-        },
-        components: {
-            TopNavbar,
-            Sidebar
-        },
-        methods:{
-            addItem: function(){
-                this.items.push({
-                    "name":this.newItem,
-                    "Quantity": this.newItemQty
-                })
-
-                this.newItem = "";
-                this.newItemQty = 0;
-            }
+export default {
+    data() {
+        return {
+            department: "Finance",
+            organization: "SMU",
+            newItem: "",
+            newItemQty: 0,
+            items: [
+                {
+                    "name": "Ikea Dalgon",
+                    "Quantity": 4
+                },
+                {
+                    "name": "Swiss Candace",
+                    "Quantity": 10
+                },
+                {
+                    "name": "Woo Jablomi",
+                    "Quantity": 5
+                },
+                {
+                    "name": "Big ForceKin",
+                    "Quantity": 10
+                },
+                {
+                    "name": "Vicky",
+                    "Quantity": 1
+                },
+            ]
         }
-        
+    },
+    components: {
+        TopNavbar,
+        Sidebar
+    },
+    methods: {
+        addItem: function () {
+            this.items.push({
+                "name": this.newItem,
+                "Quantity": this.newItemQty
+            })
+
+            this.newItem = "";
+            this.newItemQty = 0;
+        }
     }
+
+}
 
 </script>
