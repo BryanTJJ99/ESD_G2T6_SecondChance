4c4638f27627d749ad478c46ae19525ae98e92e1
JustinPansacola <pansacolajustin@gmail.com>
2023-03-18 00:24:19 +0800

    Update Inventory.vue

--- frontend/src/views/Inventory.vue
+++ frontend/src/views/Inventory.vue
@@ -27,10 +27,20 @@
                 <div class="list-group px-3">
                     <button v-for="item in items" :key="item.id" type="button"
                         class="list-group-item list-group-item-action d-flex justify-content-between p-3 pt-3 ps-3">
-                        <p>{{ item['name'] }}</p>
-                        <span class="badge" style="background-color:#c5dad2; color: black; width: 100px;">
-                            <p>Quantity : {{ item['Quantity'] }}</p>
-                        </span>
+
+                        <p>{{ item['name'] }} &nbsp; 
+                            <span>
+                                <small> &nbsp;  {{ item['category'] }}</small>
+                            </span>
+                        </p>
+
+                        <div class="align-items-center">
+                            <span class="badge" style="background-color:#c5dad2; color: black; width: 100px;">
+                            <p>Quantity : {{ item['Quantity'] }}</p></span>
+                            <i class="fa-regular fa-store mx-3" data-bs-toggle="modal"
+                            data-bs-target="#marketModal"></i>
+                        </div>
+                        
                     </button>
 
 
@@ -70,6 +80,37 @@
             </div>
         </div>
 
+        <div class="modal fade" id="marketModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5 title" id="exampleModalLabel">Sell Item</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="background-color:#c5dad2;" id="newItemForm">
+                                <p>Item Name</p>
+                            </span>
+                            <input type="text" v-model="newItem" class="form-control" placeholder="Item Name"
+                                aria-label="itemName" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="background-color:#c5dad2;" id="newItemQty">
+                                <p>Quantity</p>
+                            </span>
+                            <input type="number" v-model="newItemQty" class="form-control" placeholder="Qty. No"
+                                aria-label="newItemQty" aria-describedby="basic-addon1">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                        <button class="btn btn-dark" type="button" @click="addItem">Post On Marketplace</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -88,22 +129,27 @@ export default {
             items: [
                 {
                     "name": "Ikea Dalgon",
+                    "category": "Chair",
                     "Quantity": 4
                 },
                 {
                     "name": "Swiss Candace",
+                    "category": "Chair",
                     "Quantity": 10
                 },
                 {
                     "name": "Woo Jablomi",
+                    "category": "Chair",
                     "Quantity": 5
                 },
                 {
                     "name": "Big ForceKin",
+                    "category": "Table",
                     "Quantity": 10
                 },
                 {
                     "name": "Vicky",
+                    "category": "Office Supplies",
                     "Quantity": 1
                 },
             ]
