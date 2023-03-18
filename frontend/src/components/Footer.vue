b493804ce987e61b7a902fd7d5d28a9cb0fad997
sclaireanalisa25 <claireas.2021@smu.edu.sg>
2023-03-18 16:13:24 +0800

    final final footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -10,25 +10,16 @@
               <div class="col-lg-8">
                 <p class="mt-3 mb-2" style="color:#6e9190">
                   SecondChance is built on the concept of 'the circular economy' which involves repairing and recycling existing products for as long as possible.
-                  We have developed a platform that enables every member of your organization to optimize the use of available resources and facilitate the exchange of goods when necessary.
+                  We have developed a platform that enables every member of your organization to optimize the use of available resources and facilitate the exchange of goods.
                 </p>
-              </div>
-            </div>
-          </section>
-          <!-- Section: Text -->
-
-          <!-- Section: CTA -->
-          <section class="pb-2">
-            <p class="d-flex justify-content-center align-items-center">
-              <span class="me-3">
-                <div class="col col-sm-4 d-flex align-items-center px-4">
+                <div class="d-flex justify-content-center px-4 mb-2">
                   <img class="ms-4 me-1" src="../assets/logo.png" style="height:20px;opacity:50%;">
                   <router-link to="/" class="nav-link text-dark m-0 p-0" >
                     <p class="logo ">SecondChance(2023)</p>
                   </router-link>
                 </div>
-              </span>
-            </p>
+              </div>
+            </div>
           </section>
         <!-- Grid container -->
 
