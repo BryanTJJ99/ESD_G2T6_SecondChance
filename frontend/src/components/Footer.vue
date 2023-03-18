0804cb18244c5fa75870df211f3fc96a6969e051
sclaireanalisa25 <claireas.2021@smu.edu.sg>
2023-03-18 15:03:41 +0800

    footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,23 +1,77 @@
-<style>
-.footer {
-  background-color:#c5dad2;
-  z-index:2;
-  margin-left: 4.5rem;
-  box-shadow:0 0 8px 8px rgba(208, 207, 207, 0.3);
-  margin-bottom: 0;
-  margin-top: 1.5rem;
-  width: 100%;
-  font-family: sans-serif;
-
-}
-</style>
 
 <template>
-    <div class="pa-2 footer">
+    <!-- <div class="px-5 footer py-4 py-md-5 mt-5">
+        <div class="row px-5">
+          <div class="col col-sm-4 d-flex align-items-center px-4">
+            <img class="ms-4 me-1" src="../assets/logo.png" style="height:40px;opacity:50%;">
+            <router-link to="/" class="nav-link text-dark m-0 p-0" >
+              <h3 class="logo ">SecondChance</h3>
+            </router-link>
+          </div>
+          <div class="col col-sm-4">
+            <h5 class="mt-2">USEFUL LINKS</h5>
+          </div>
+          <div class="col col-sm-4">
+            <h5 class="mt-2">CONTACT US</h5>
+          </div>
+        </div>
+
+        <div class="row px-5">
+          <div class="col col-sm-auto d-flex align-items-center">
+            <p>Here you can use rows to organise your footer content</p>
+          </div>
+          <div class="col col-sm-auto">
+
+          </div>
+          <div class="col col-sm-auto">
+          </div>
+        </div>
+    </div> -->
+    <!-- <div class="pa-2 footer">
         <strong style="color:#1c5930">This website is made by Team SecondChance</strong>
         <br>
         <small style="color:#1c5930">Our team consists of Aloysius, Analisa, Bryan, Jordan, Justin and Vicky</small>
-    </div>
+    </div> -->
+    <!-- Remove the container if you want to extend the Footer to full width. -->
+
+      <!-- Footer -->
+      <footer class="text-center text-white" style="background-color: #c5dad2; margin-left:4.5rem">
+        <!-- Grid container -->
+        
+    
+          <!-- Section: Text -->
+          <section>
+            <div class="row d-flex justify-content-center">
+              <div class="col-lg-8">
+                <p class="mt-3 mb-2" style="color:#6e9190">
+                  SecondChance is built on the concept of 'the circular economy' which involves repairing and recycling existing products for as long as possible.
+                  We have developed a platform that enables every member of your organization to optimize the use of available resources and facilitate the exchange of goods when necessary.
+                </p>
+              </div>
+            </div>
+          </section>
+          <!-- Section: Text -->
+
+          <!-- Section: CTA -->
+          <section class="pb-2">
+            <p class="d-flex justify-content-center align-items-center">
+              <span class="me-3">
+                <div class="col col-sm-4 d-flex align-items-center px-4">
+                  <img class="ms-4 me-1" src="../assets/logo.png" style="height:20px;opacity:50%;">
+                  <router-link to="/" class="nav-link text-dark m-0 p-0" >
+                    <p class="logo ">SecondChance(2023)</p>
+                  </router-link>
+                </div>
+              </span>
+            </p>
+          </section>
+        <!-- Grid container -->
+
+      </footer>
+      <!-- Footer -->
+
+    <!-- End of .container -->
+<!-- End of .container -->
 </template>
 
 <script>
