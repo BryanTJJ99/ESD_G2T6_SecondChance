89be12bf90421ed42f7faa7268f4b70f115960dd
Jordan Tay <jordantay226@gmail.com>
2023-03-18 09:49:15 +0800

    added leaderboard to kafka exchange

--- backend/kafka-leaderboard/consumer-leaderboard.py
+++ backend/kafka-leaderboard/consumer-leaderboard.py
@@ -1,8 +1,11 @@
 from kafka import KafkaConsumer
+import json
 
 TOPIC_NAME = 'leaderboard'
 
 consumer = KafkaConsumer(TOPIC_NAME)
+consumer = KafkaConsumer(TOPIC_NAME, value_deserializer=lambda v: json.loads(v.decode('utf-8')))
+
 for message in consumer:
     print(message)
 
--- backend/kafka-leaderboard/producer-leaderboard.py
+++ backend/kafka-leaderboard/producer-leaderboard.py
@@ -1,9 +1,10 @@
 from kafka import KafkaProducer
+import json
 
 TOPIC_NAME = 'leaderboard'
 KAFKA_SERVER = 'localhost:9092'
 
-producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
-
-producer.send(TOPIC_NAME, b' hi bryan!!!!')
+producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'), bootstrap_servers=KAFKA_SERVER)
+message = {'department':'finance','score':100}
+producer.send(TOPIC_NAME, message)
 producer.flush()
