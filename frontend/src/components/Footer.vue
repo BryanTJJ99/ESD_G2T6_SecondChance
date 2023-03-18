d1a807dcd0cffe24bccb9286ae0147c7bd445a4c
sclaireanalisa25 <claireas.2021@smu.edu.sg>
2023-03-18 15:52:06 +0800

    leaderboard border rounded

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,37 +1,5 @@
 
 <template>
-    <!-- <div class="px-5 footer py-4 py-md-5 mt-5">
-        <div class="row px-5">
-          <div class="col col-sm-4 d-flex align-items-center px-4">
-            <img class="ms-4 me-1" src="../assets/logo.png" style="height:40px;opacity:50%;">
-            <router-link to="/" class="nav-link text-dark m-0 p-0" >
-              <h3 class="logo ">SecondChance</h3>
-            </router-link>
-          </div>
-          <div class="col col-sm-4">
-            <h5 class="mt-2">USEFUL LINKS</h5>
-          </div>
-          <div class="col col-sm-4">
-            <h5 class="mt-2">CONTACT US</h5>
-          </div>
-        </div>
-
-        <div class="row px-5">
-          <div class="col col-sm-auto d-flex align-items-center">
-            <p>Here you can use rows to organise your footer content</p>
-          </div>
-          <div class="col col-sm-auto">
-
-          </div>
-          <div class="col col-sm-auto">
-          </div>
-        </div>
-    </div> -->
-    <!-- <div class="pa-2 footer">
-        <strong style="color:#1c5930">This website is made by Team SecondChance</strong>
-        <br>
-        <small style="color:#1c5930">Our team consists of Aloysius, Analisa, Bryan, Jordan, Justin and Vicky</small>
-    </div> -->
     <!-- Remove the container if you want to extend the Footer to full width. -->
 
       <!-- Footer -->
