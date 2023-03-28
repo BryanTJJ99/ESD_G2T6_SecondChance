ecafcb4aeca46f7121cc834041ebfe6a362a5d1f
Bryan Tan Jia Jun <bryan.tanjiajun@gmail.com>
2023-03-28 20:39:33 +0800

    deleted password and email in department, added kong api gateway folder

--- backend/department/src/main/java/com/ESDBackend/department/models/Department.java
+++ backend/department/src/main/java/com/ESDBackend/department/models/Department.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,49 +13,41 @@ import lombok.Setter;
 @Setter
 @Document(collection = "departments")
 public class Department {
-    
+
     @Id
     private String departmentId;
     private String departmentName;
     private String companyId;
-    private String email;
     private List<String> itemIdArrayList;
-    private String password;
     private String postalCode;
     private double totalCarbon;
-    
 
     // public Department() {
-    //     // Default constructor
+    // // Default constructor
     // }
-    // public Department(String departmentName, String country, String postalCode, double totalCarbon) {
-    //     this.departmentName = departmentName;
-    //     this.departmentCountry = country;
-    //     this.departmentPostalCode = postalCode;
-    //     this.totalCarbon = totalCarbon;
-    //     this.itemIdArrayList = new ArrayList<String>();
+    // public Department(String departmentName, String country, String postalCode,
+    // double totalCarbon) {
+    // this.departmentName = departmentName;
+    // this.departmentCountry = country;
+    // this.departmentPostalCode = postalCode;
+    // this.totalCarbon = totalCarbon;
+    // this.itemIdArrayList = new ArrayList<String>();
     // }
-    
+
     public Department() {
 
     }
 
-
     // public List<String> getItemsIdArr() {
-    //     return itemIdArrayList;
+    // return itemIdArrayList;
     // }
 
-    
-    public void addItem(String itemID){
+    public void addItem(String itemID) {
         itemIdArrayList.add(itemID);
     }
-    
 
-    public void removeItem(String itemID){
+    public void removeItem(String itemID) {
         itemIdArrayList.remove(itemID);
     }
 
-
 }
-
-
