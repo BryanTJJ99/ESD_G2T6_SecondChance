62e4dabdb71008ca59f12e86c748ebd366d84e47
vickyyqu <112297073+vickyyqu@users.noreply.github.com>
2023-04-03 00:10:12 +0800

    trying registration T.T

---
+++ frontend/services/register/registerService.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+const DEPT_URL = 'http://localhost:8080/department'
+const COMPANY_URL = 'http://localhost:5001'
+
+class registerService{
+
+    // ADD NEW DEPARTMENT
+    addDepartment(dept){
+
+        const response = axios
+        .post(DEPT_URL + '/create', dept)
+            .then(response => {
+    
+                // console.log(response.data)
+                console.log("dept added successfully")
+
+                return response
+    
+            })
+            .catch(error => {
+                console.log(error.message)
+    
+            })
+    }
+
+    addCompany(company){
+        const response = axios
+        .post(COMPANY_URL + "/create", company)
+            .then(response => {
+                // console.log(response.data)
+                console.log("company added successfully")
+
+                return response
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+    }
+
+    updateDepartment(dept, deptId){
+        const response = axios
+        .put(DEPT_URL + "/update/" + deptId, dept
+    )
+        .then(response => {
+
+            console.log(response.data)
+            console.log("dept modified successfully")
+        })
+        .catch(error => {
+            console.log(error.message)
+
+        })
+
+    }
+
+    updateCompany(company, companyId){
+        const response = axios
+        .put(COMPANY_URL + "/edit/" + companyId, company
+    )
+        .then(response => {
+
+            console.log(response.data)
+            console.log("company modified successfully")
+        })
+        .catch(error => {
+            console.log(error.message)
+
+        })
+
+    }
+
+}
+export default new registerService;
